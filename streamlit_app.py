bffd286da3caf79136ee013a731817cf6f65b24a
Sakshi Bansal <104644713+sak1990@users.noreply.github.com>
2022-04-30 15:32:59 +0530

    Update streamlit_app.py
    Filtered the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 # let's pick a pick list for user to choose a fruit by themselve
-streamlit.multiselect("Pick fruits of your choice:" , list( my_fruit_list.index), ['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruit_selected = streamlit.multiselect("Pick fruits of your choice:" , list( my_fruit_list.index), ['Avocado','Strawberries'])
+fruit_selected_loc = my_fruit_list.loc[fruit_selected]
+streamlit.dataframe(fruit_selected_loc)
