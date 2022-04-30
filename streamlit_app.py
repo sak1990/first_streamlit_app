8b5ee1c808bc6d9b4f188b86db55dc92e6da6730
Sakshi Bansal <104644713+sak1990@users.noreply.github.com>
2022-04-30 15:24:17 +0530

    Update streamlit_app.py
    Added a list picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# let's pick a pick list for user to choose a fruit by themselve
+streamlit.multiselect("Pick fruits of your choice:" , list( my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
