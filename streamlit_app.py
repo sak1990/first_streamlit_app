0d4d3906bc1d31ee95d6d5dd6eb9801b1eaa9229
Sakshi Bansal <104644713+sak1990@users.noreply.github.com>
2022-04-30 15:27:02 +0530

    Update streamlit_app.py
    Index set to Fruit instead of numbers

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 # let's pick a pick list for user to choose a fruit by themselve
 streamlit.multiselect("Pick fruits of your choice:" , list( my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
